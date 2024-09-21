b85a5e4796d79ab6df97f987411fca76ee485d3d
qoride <qoride@gmail.com>
2024-09-21 00:19:19 +0000

    symbol testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(){
+    const string charMap[] = {"  ","░░","▒▒","▓▓","██"};
+    string input;
+
+    cout << "Type \"list\" to see the list of patterns, \"help\" for help on how to format an input, or \"quit\" to stop the program.";
 
     return 0;
 }
